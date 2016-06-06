885cfa4feedbad250f507e62c133a138d0f27440
Dmitry Avvakumov <dima.avvakumov@gmail.com>
2016-06-06 10:31:44 +0500

    UIKit dependency fixed

--- ALLocalizedManager/ALLocalizedManager.h
+++ ALLocalizedManager/ALLocalizedManager.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #define UserdefaultsKey_Lang @"UserdefaultsKey_Lang"
 #define ALLocalizedManagerChangeLangNotification @"ALLocalizedManagerChangeLangNotification"
